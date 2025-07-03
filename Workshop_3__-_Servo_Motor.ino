b77b73def447d2bec726e6aab66c6ce17c80237b
Tball-eng <tball@uwaterloo.ca>
2025-07-03 11:57:42 -0400

    Workshop_3__-_Servo_Motor.ino- New Comments
    Added an addtional comment to the code

--- Workshop_3__-_Servo_Motor.ino
+++ Workshop_3__-_Servo_Motor.ino
@@ -1,6 +1,8 @@
 #include <Servo.h>
+// Hi this is my new comment/ edit: Testing anything
 
 Servo myservo1;
+Servo myservo2;
 int currentPosition = 0; 
 
 void setup() {
@@ -10,6 +12,11 @@ void setup() {
   myservo1.attach(11); //configure digital pin 11 to control the servo
   myservo1.write(0);
   delay(1500);
+
+  // === Setup Code for Part 1 (b) === //
+  //myservo2.attach(9); //configure digital pin 9 to control the servo
+  //myservo2.write(93);
+  //delay(1000);
 }
 
 void posServo(){
@@ -20,8 +27,37 @@ void posServo(){
 }
   
 
+/*
+void rotServo(){
+  
+  
+  int num = Serial.parseInt();
+  Serial.println(num);
+  delay(100);
+  
+  if (num == 0){
+    myservo2.write(93);
+    delay(1000);
+  }
+  
+  else if ( 0 < num <= 100){
+    myservo2.write(map(num, 0, 100, 93, 0));
+    delay(1000);
+  }
+  
+  else if ( -100 <= num < 0) {
+    myservo2.write(map(num, -100, 0, 180, 93)); 
+    delay(1000);
+  }
+ 
+}
+  */ 
+
 void loop() {
-  posServo(); //For Position Servo
+  // Call the function for each part of the lab here to run
+
+   posServo(); //For Position Servo
+  // rotServo(); //For Continuous Rotation Servo
   delay(5);
 
 }
