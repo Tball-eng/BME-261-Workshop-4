7ecf084f4da0469cb64a06f7facc65d4651e55ae
Tball-eng <tball@uwaterloo.ca>
2025-07-03 11:39:25 -0400

    Add files via upload

---
+++ Workshop_3__-_Servo_Motor.ino
@@ -0,0 +1,63 @@
+#include <Servo.h>
+
+
+Servo myservo1;
+Servo myservo2;
+int currentPosition = 0; 
+
+void setup() {
+  Serial.begin(9600);
+
+  // === Setup Code for Part 1 (a) === //
+  myservo1.attach(11); //configure digital pin 11 to control the servo
+  myservo1.write(0);
+  delay(1500);
+
+  // === Setup Code for Part 1 (b) === //
+  //myservo2.attach(9); //configure digital pin 9 to control the servo
+  //myservo2.write(93);
+  //delay(1000);
+}
+
+void posServo(){
+  int num = Serial.parseInt();
+    Serial.println(num);
+    myservo1.write(num);
+    delay(100);
+}
+  
+
+/*
+void rotServo(){
+  
+  
+  int num = Serial.parseInt();
+  Serial.println(num);
+  delay(100);
+  
+  if (num == 0){
+    myservo2.write(93);
+    delay(1000);
+  }
+  
+  else if ( 0 < num <= 100){
+    myservo2.write(map(num, 0, 100, 93, 0));
+    delay(1000);
+  }
+  
+  else if ( -100 <= num < 0) {
+    myservo2.write(map(num, -100, 0, 180, 93)); 
+    delay(1000);
+  }
+ 
+}
+  */ 
+
+void loop() {
+  // Call the function for each part of the lab here to run
+
+   posServo(); //For Position Servo
+  // rotServo(); //For Continuous Rotation Servo
+  delay(5);
+
+}
