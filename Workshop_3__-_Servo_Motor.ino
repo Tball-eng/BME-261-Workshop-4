9cf70c55eed957b44105dbb3553174346c8f4abe
Tball-eng <tball@uwaterloo.ca>
2025-07-03 11:54:28 -0400

    Workshop_3__-_Servo_Motor.ino - Postional Motor Only
    Contains only the code for the postional Motor

--- Workshop_3__-_Servo_Motor.ino
+++ Workshop_3__-_Servo_Motor.ino
@@ -1,44 +1,27 @@
 #include <Servo.h>
 
-Servo myservo2;
+Servo myservo1;
 int currentPosition = 0; 
 
 void setup() {
   Serial.begin(9600);
 
-  // === Setup Code for Part 1 (b) === //
-  myservo2.attach(9); //configure digital pin 9 to control the servo
-  myservo2.write(93);
-  delay(1000);
+  // === Setup Code for Part 1 (a) === //
+  myservo1.attach(11); //configure digital pin 11 to control the servo
+  myservo1.write(0);
+  delay(1500);
 }
 
-  
-void rotServo(){
-  
+void posServo(){
   int num = Serial.parseInt();
-  Serial.println(num);
-  delay(100);
-  
-  if (num == 0){
-    myservo2.write(93);
-    delay(1000);
-  }
-  
-  else if ( 0 < num <= 100){
-    myservo2.write(map(num, 0, 100, 93, 0));
-    delay(1000);
-  }
-  
-  else if ( -100 <= num < 0) {
-    myservo2.write(map(num, -100, 0, 180, 93)); 
-    delay(1000);
-  }
- 
+    Serial.println(num);
+    myservo1.write(num);
+    delay(100);
 }
+  
 
 void loop() {
-  // Call the function for each part of the lab here to run
-  rotServo(); //For Continuous Rotation Servo
+  posServo(); //For Position Servo
   delay(5);
 
 }
